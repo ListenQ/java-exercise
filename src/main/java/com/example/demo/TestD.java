44973d85e5aba97b88d1be4d1f8f9be3be4f4a68
zhangqi1214 <zhangqi@wxdl.com>
2019-03-13 18:28:22 +0800

    阿萨德发

--- src/main/java/com/example/demo/TestD.java
+++ src/main/java/com/example/demo/TestD.java
@@ -16,9 +16,9 @@ public class TestD {
 		List<Long> ids2 = new ArrayList<>(0);
 		Long id = 454l;
 		//System.out.println(Arrays.equals(num[0],w));
-		System.out.println(ids.contains(id));
+		//System.out.println(ids.contains(id));
 		String s[] = "|Wechat|feixiaohao07|copy".split("\\|");
-		System.out.println(s[2]);
+		//System.out.println(s[2]);
 		
 //		for (int i = 0; i < 10; i++) {
 //			int nextInt = RandomUtils.nextInt(6);
@@ -29,10 +29,12 @@ public class TestD {
 //			System.out.println(isd);
 //		});
 		
-//		String ss= "  测试标题&casd& #40; asdf & #41;  ";
-//		System.out.println(StringEscapeUtils.unescapeHtml4(ss.replaceAll("\\s*","")));
+//		String ss= "&#22899;&#31070;&#33410;&#36731;&#26494;get&radic;&#33459;&#24515;&#30340;&#31192;&#35776;&#65281;TF&#21475;&#32418;&#12289;&#31062;&#39532;&#40857;&#39321;&#27700;&#12289;LA MER&#30524;&#38684;&hellip;&hellip;&#22899;&#31070;&#30340;&#24515;&#24605;&#24110;&#20320;&#25720;&#36879;&#20102;&#65292;&#24555;&#36190;&#25105;&#65281;"; 
+//		System.out.println(StringEscapeUtils.unescapeHtml4(ss));
 		
-		System.out.println("bqi_web".contains("fxh"));
+//		System.out.println("bqi_web".contains("fxh"));
+		
+		
 		
 	}
 
