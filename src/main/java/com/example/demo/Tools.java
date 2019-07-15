b7029dcd92b06bc73c5b07b24a716b9793d80b7b
zhangqi1214 <zhangqi@wxdl.com>
2019-07-15 14:42:05 +0800

    完成jwt token生成规则

--- src/main/java/com/example/demo/Tools.java
+++ src/main/java/com/example/demo/Tools.java
@@ -8,25 +8,11 @@ import javax.crypto.spec.SecretKeySpec;
 public class Tools {
 
 	public static String HMACSHA256(String data, String key) throws Exception {
-
 		Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
-
-		SecretKeySpec secret_key = new SecretKeySpec(key.getBytes(), "HmacSHA256");
-
+		SecretKeySpec secret_key = new SecretKeySpec(key.getBytes("UTF-8"), "HmacSHA256");
 		sha256_HMAC.init(secret_key);
-
-		byte[] array = sha256_HMAC.doFinal(data.getBytes());
-
-		StringBuilder sb = new StringBuilder();
-
-		for (byte item : array) {
-
-			sb.append(Integer.toHexString((item & 0xFF) | 0x100).substring(1, 3));
-
-		}
-	
-		return  Base64.getEncoder().encodeToString(sb.toString().getBytes());
-
+		byte[] array = sha256_HMAC.doFinal(data.getBytes("UTF-8"));
+		return Base64.getEncoder().encodeToString(array).replaceAll("=", "");
 	}
   
 }
