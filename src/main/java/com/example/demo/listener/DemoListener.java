cd704bd57847f58eb798a915452bede402a54eff
zhangqi <zhangqi@admin.com>
2020-06-29 13:47:09 +0800

    添加

--- src/main/java/com/example/demo/listener/DemoListener.java
+++ src/main/java/com/example/demo/listener/DemoListener.java
@@ -7,6 +7,13 @@ import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * EventListener 是基于 Observer模式
+ * DemoListener
+ * @author zhangqi 
+ * @date 2020年6月23日-下午5:47:06
+ * 
+ */
 @Component
 @Slf4j
 public class DemoListener {
@@ -21,18 +28,17 @@ public class DemoListener {
 	@EventListener(classes= {Demo2Event.class})
 	public void listener2Event(Demo2Event event2) throws IOException {
 		log.info("event2监听来了{}",event2.getMessage());
-		listenerHandler(2);
+		listenerHandler(1);
 	}
 	
 	@EventListener(classes= {Demo3Event.class})
 	public void listener3Event(Demo2Event event3) throws IOException {
 		log.info("event3监听来了{}",event3.getMessage());
-		listenerHandler(3);
+		listenerHandler(1);
 	}
 	
 	
 	public void listenerHandler(int number) throws IOException {
-		
-		System.err.println("事情做完了");
+		System.err.println("事情做完了,发送去客户端");
 	}
 }
