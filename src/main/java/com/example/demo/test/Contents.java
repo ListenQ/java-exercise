b49aef62502983bf61e1e95250b919c8ffca578f
admin <admin@admin-PC>
2020-07-18 09:42:38 +0800

    tj

--- src/main/java/com/example/demo/test/Contents.java
+++ src/main/java/com/example/demo/test/Contents.java
@@ -7,5 +7,13 @@ public interface Contents {
 		public static final Integer DOC_OPEN_URL = 0;
 		public static final Integer DOC_OBS_URL  = 1;
 	}
+	
+	static interface CM {
+		String A = "dsfs@request_queue";
+		String B = "kljk@request_queue";
+		String C = "qwe@request_queue";
+		String D = "bnm@request_queue";
+		String E = "fgh@request_queue";
+	}
 
 }
---
+++ src/main/java/com/example/demo/test/Test12.java
@@ -0,0 +1,21 @@
+package com.example.demo.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test12 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	static class CMSingln{
+		private static Map<String,String> map = new HashMap<>();
+		
+		private CMSingln() {}
+		
+		
+	}
+	
+	
+}
