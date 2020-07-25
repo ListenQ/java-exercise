2f4391eb968f10549bb93f2a3afb48fd750326e0
admin <admin@admin-PC>
2020-07-25 22:56:16 +0800

    tj

--- src/main/java/com/example/demo/design/patterns/strategy/Context.java
+++ src/main/java/com/example/demo/design/patterns/strategy/Context.java
@@ -1,5 +1,13 @@
 package com.example.demo.design.patterns.strategy;
 
+/**
+ * 策略模式
+ * Context<BR>
+ * 创建人：zhangqi <BR>
+ * 时间：2020年7月25日-下午10:53:08 <BR>
+ * @version 1.0.0
+ * 
+ */
 public class Context {
 	
 	private Strategy strategy;
