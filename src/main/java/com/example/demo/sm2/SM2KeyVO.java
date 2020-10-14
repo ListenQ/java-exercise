3e1056e8ea959cbc99ae01a2551af2ab240bc31c
zhangqi <zhangqi@admin.com>
2020-10-14 14:42:54 +0800

    修改

--- src/main/java/com/example/demo/sm2/SM2KeyVO.java
+++ src/main/java/com/example/demo/sm2/SM2KeyVO.java
@@ -1,42 +1,42 @@
-package com.example.demo.sm2;
-
-import java.math.BigInteger;
-
-import org.bouncycastle.math.ec.ECPoint;
-
-import com.example.demo.util.Util;
-
-public class SM2KeyVO {
-    BigInteger privateKey ;
-    ECPoint publicKey ;
-
-    public BigInteger getPrivateKey() {
-        return privateKey;
-    }
-
-    public void setPrivateKey(BigInteger privateKey) {
-        this.privateKey = privateKey;
-    }
-
-    public ECPoint getPublicKey() {
-        return publicKey;
-    }
-
-    public void setPublicKey(ECPoint publicKey) {
-        this.publicKey = publicKey;
-    }
-
-
-    //HardPubKey:3059301306072A8648CE3D020106082A811CCF5501822D03420004+X+Y
-    //SoftPubKey:04+X+Y
-    public String getPubHexInSoft(){
-        return Util.byteToHex(publicKey.getEncoded(true));
-        //System.out.println("公钥: " + );
-    }
-    public String getPubHexInHard(){
-        return SecurityTestAll.SM2PubHardKeyHead +Util.byteToHex(publicKey.getEncoded(true));
-    }
-    public String getPriHexInSoft(){
-        return Util.byteToHex(privateKey.toByteArray());
-    }
-}
+//package com.example.demo.sm2;
+//
+//import java.math.BigInteger;
+//
+//import org.bouncycastle.math.ec.ECPoint;
+//
+//import com.example.demo.util.Util;
+//
+//public class SM2KeyVO {
+//    BigInteger privateKey ;
+//    ECPoint publicKey ;
+//
+//    public BigInteger getPrivateKey() {
+//        return privateKey;
+//    }
+//
+//    public void setPrivateKey(BigInteger privateKey) {
+//        this.privateKey = privateKey;
+//    }
+//
+//    public ECPoint getPublicKey() {
+//        return publicKey;
+//    }
+//
+//    public void setPublicKey(ECPoint publicKey) {
+//        this.publicKey = publicKey;
+//    }
+//
+//
+//    //HardPubKey:3059301306072A8648CE3D020106082A811CCF5501822D03420004+X+Y
+//    //SoftPubKey:04+X+Y
+//    public String getPubHexInSoft(){
+//        return Util.byteToHex(publicKey.getEncoded(true));
+//        //System.out.println("公钥: " + );
+//    }
+//    public String getPubHexInHard(){
+//        return SecurityTestAll.SM2PubHardKeyHead +Util.byteToHex(publicKey.getEncoded(true));
+//    }
+//    public String getPriHexInSoft(){
+//        return Util.byteToHex(privateKey.toByteArray());
+//    }
+//}
--- src/main/java/com/example/demo/test/Test15.java
+++ src/main/java/com/example/demo/test/Test15.java
@@ -1,16 +1,42 @@
 package com.example.demo.test;
 
+import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Test15 {
 	
 	public static void main(String[] args) {
-		try {
-			System.out.println(1/0);
-		} catch (Throwable e) {
-			log.error("异常:{},{}","45",e);
-		}
+//		try {
+//			System.out.println(1/0);
+//		} catch (Throwable e) {
+//			log.error("异常:{},{}","45",e);
+//		}
+		
+		String s = " {\r\n" + 
+				"    \"header\":{\r\n" + 
+				"        \"reqId\":\"8bde5d19-1b9a-4b76-8557-b4bec3f356cd\",\r\n" + 
+				"        \"path\":\"auth.auth\",\r\n" + 
+				"        \"language\":\"ZN\",\r\n" + 
+				"        \"version\":\"1.0.0\"\r\n" + 
+				"    },\r\n" + 
+				"    \"body\":{\r\n" + 
+				"        \"deviceId\":\"4baf90f9-84c2-405f-bf25-378988d3c009\",\r\n" + 
+				"        \"timestamp\":1586866102935,\r\n" + 
+				"        \"token\":\"zq666666\",\r\n" + 
+				"        \"osType\":\"ios\",\r\n" + 
+				"		\"accCode\":\"T870886080\",\r\n" + 
+				"		\"pwd\":\"123456\"\r\n" + 
+				"    }\r\n" + 
+				"  }";
+		
+		 s = "{阿斯顿发生asdf:1245}";
+		System.out.println(StrUtil.subAfter(s,"asdf",true));
+		System.out.println(StrUtil.subPre(s,s.indexOf("kk")));
+		System.out.println(StrUtil.subBetween(s, ":", "}"));
+		System.out.println(StrUtil.unWrap(s, ":", "}"));
 	}
+	
+
 
 }
