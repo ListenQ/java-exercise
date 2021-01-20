22c14deed2635b825358dfb936c9e74415902614
zhangqi <zhangqi@admin.com>
2021-01-20 11:03:18 +0800

    修改

---
+++ src/main/java/com/example/demo/algorithm/Ctest_LongestCommonPrefix.java
@@ -0,0 +1,34 @@
+package com.example.demo.algorithm;
+
+/**
+ * 最长公共前缀
+ * Ctest_LongestCommonPrefix
+ * @author zhangqi 
+ * @date 2021年1月16日-上午10:48:38
+ *
+ */
+public class Ctest_LongestCommonPrefix {
+	
+	public static void main(String[] args) {
+		long start = System.nanoTime();
+		String[] strs = {"",""};
+		String result = longestCommonPrefix(strs );
+		System.out.println((System.nanoTime() - start)+"*result = "+result);
+	}
+	
+	
+	/**
+	 * 编写一个函数来查找字符串数组中的最长公共前缀，如果不存在公共前缀，返回空字符串 ""
+	 */
+	public static String longestCommonPrefix(String[] strs) {
+		String longPrefix = "";
+		if (null == strs || strs.length == 0) {
+			return longPrefix;
+		}
+		for (int i = 0; i < strs.length; i++) {
+			
+		}
+		return longPrefix;
+    }
+
+}
--- src/main/java/com/example/demo/test/Test16.java
+++ src/main/java/com/example/demo/test/Test16.java
@@ -1,14 +1,52 @@
 package com.example.demo.test;
 
-import java.math.BigDecimal;
+import cn.hutool.extra.cglib.CglibUtil;
+import lombok.Getter;
+import lombok.Setter;
 
 public class Test16 {
 	
 	public static void main(String[] args) {
-		BigDecimal a = new BigDecimal("10");
-		BigDecimal b = new BigDecimal("20");
-		BigDecimal c = new BigDecimal("200");
-		System.out.println(a.subtract(b).multiply(c));
+		User u = new User();
+		u.setAge(21);
+		u.setName("李四");
+		
+		Person p = new Person();
+//		p.setName(null);
+		p.setAddress("深圳南山");
+		
+		CglibUtil.copy(p, u);
+		
+		System.out.println(u);
+	}
+	
+	@Getter
+	@Setter
+	static class User {
+		private Integer age;
+		
+		private String name;
+		
+		private String address;
+
+		@Override
+		public String toString() {
+			return "User [age=" + age + ", name=" + name + ", address=" + address + "]";
+		}
+		
+	}
+	
+	@Getter
+	@Setter
+	static class Person {
+		private String name;
+		
+		private String address;
+
+		@Override
+		public String toString() {
+			return "Person [name=" + name + ", address=" + address + "]";
+		}
 	}
 
 }
