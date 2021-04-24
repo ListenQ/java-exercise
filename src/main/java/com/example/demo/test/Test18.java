598a4e42dd35c851e68b726e625a682ad287f0ce
admin <admin@admin-PC>
2021-04-24 19:48:36 +0800

    tj

--- src/main/java/com/example/demo/test/Test18.java
+++ src/main/java/com/example/demo/test/Test18.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.example.demo.test.Test16.Person;
 import com.example.demo.util.JacksonUtil;
@@ -22,14 +24,25 @@ public class Test18 {
 		person.setAmount(new BigDecimal("236.5454"));
 //		System.out.println(person.getAmount().negate());
 		
-		List<String> a = new ArrayList<>();
-		a.add("132");
-		List<String> b = new ArrayList<>();
-		b.add("465");
+		// 1 2 持有 3已经清仓 4 中签IPO
+		List<Integer> a = new ArrayList<>();
+		a.add(1);
+		a.add(2);
+		a.add(4);
+		List<Integer> b = new ArrayList<>();
+		b.add(1);
+		b.add(2);
+		b.add(3);
+		
+		
+		List<Integer> collect = a.stream().filter(l -> b.contains(l)).collect(Collectors.toList());
+		System.out.println(collect);
+		List<Integer> collect2 = Stream.of(a,b).flatMap(x -> x.stream()).distinct().collect(Collectors.toList());
+		System.out.println(collect2);
 		
 		a = null;
 		Optional.ofNullable(a).orElse(new ArrayList<>()).addAll(b);
-		System.out.println(a);
+//		System.out.println(a);
 	}
 	
 	
