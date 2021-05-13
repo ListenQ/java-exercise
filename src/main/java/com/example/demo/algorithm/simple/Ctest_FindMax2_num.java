d95804f0e10f264ea26967c5bad98c18f4abe060
zhangqi <zhangqi@admin.com>
2021-05-13 15:46:11 +0800

    修改

--- src/main/java/com/example/demo/algorithm/simple/Ctest_FindMax2_num.java
+++ src/main/java/com/example/demo/algorithm/simple/Ctest_FindMax2_num.java
@@ -15,12 +15,21 @@ public class Ctest_FindMax2_num {
 		System.out.println(findN(N));
 	}
 	
+	
+	/**
+	 *  <a href=https://www.cnblogs.com/gjmhome/p/11351315.html  />来自地址</a> 
+	 * @author zhangqi 
+	 * @date 2021年5月13日-下午3:43:46
+	 * @param n
+	 * @return int
+	 */
 	private static int findN(int n) {
 		n |= n >> 1;
 		n |= n >> 2;
 		n |= n >> 4;
 		n |= n >> 8;
 		n |= n >> 16;
+		n |= n >> 32;
 		return (n +1 ) >>1;
 	}
 
